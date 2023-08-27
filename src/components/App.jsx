90b0d5c56ff43f2b3c5188e2d020deb650645f55
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-08-27 14:25:58 +0300

    upd1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,51 @@
-import { Feedback } from "./feedback/Feedback";
-export const App = () => {
-  return (
-    <div>
-      React homework template
-      <Feedback/>
-    </div>
-  );
+import { Component } from "react";
+import { Buttons, Counter } from "./feedback/Feedback.styled";
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+    }
+
+countUpdateState = (typeState) => {
+  this.setState((prevState) => ({
+      
+        [typeState]: prevState[typeState]+1
+
+}))
+
+}
+
+    handleClick =(typeState)=>{
+     this.countUpdateState(typeState);
+      
+      }
+
+countTotalFeedback = () => {
+
+}
+
+
+  render(){
+    return (
+      <div>
+          <h1>Please leave feedback</h1>
+          <Buttons>
+      <button onClick={() =>this.handleClick('good')}>Good</button>
+      <button onClick={() =>this.handleClick('neutral')}>Neutral</button>
+      <button onClick={() =>this.handleClick('bad')}>Bad</button>
+      </Buttons>
+      <h2>Statistics</h2>
+      <Counter>
+          <span>Good: {this.state.good}</span>
+          <span>Neutral:{this.state.neutral}</span>
+          <span>Bad:{this.state.bad}</span>
+      </Counter>
+      
+      </div>
+      
+      
+      );
+  }
 };
